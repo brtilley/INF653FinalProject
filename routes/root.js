c197725366e6c12b1d8a1ab84ebbe3304591609e
Brandon Tilley <brtilley@mail.fhsu.edu>
2025-05-01 23:27:32 -0700

    refactor: clean up code by removing unused imports and redundant comments

--- routes/root.js
+++ routes/root.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-require('dotenv').config();
-
 router.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
--- server.js
+++ server.js
@@ -55,8 +55,3 @@ mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
-
-app.use((req, res, next) => {
-    console.log(`Incoming request: ${req.method} ${req.url}`);
-    next();
-  });
