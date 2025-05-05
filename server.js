64ec6de35ca05936cb46b0efc69a0894ca01a63b
Brandon Tilley <brtilley@mail.fhsu.edu>
2025-05-04 17:13:34 -0700

    feat: refactor MongoDB connection handling to use promise-based approach

--- server.js
+++ server.js
@@ -66,9 +66,12 @@ app.use((req, res, next) => {
     next();
 });
 
-mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB database');
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+
+mongoose.connect(process.env.DATABASE_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Error connecting to MongoDB:', err);
 });
