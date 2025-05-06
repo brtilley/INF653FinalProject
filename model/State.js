273dc1a6901225a5a47ae39379e6c14884783fac
Brandon Tilley <brtilley@mail.fhsu.edu>
2025-05-05 22:51:19 -0700

    fix: update allowedOrigins and enhance request logging; remove state field requirement from State model

--- model/State.js
+++ model/State.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const stateSchema = new mongoose.Schema({
     stateCode: { type: String, required: true, unique: true },
-    state: { type: String, required: true },
     funFacts: { type: [String] }
 }, { collection: 'states' }); // Explicitly set the collection name
 
